0cdf060633f32660daf1ca62020eee3c0fff0499
shawon-MG <work.shawomg@gmail.com>
2022-05-05 00:09:19 +0600

    Get all documents from DB

--- index.js
+++ index.js
@@ -17,14 +17,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8chh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Connected with MongoDB-Atlas');
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('Connected with MongoDB-Atlas');
 
-    // perform actions on the collection object
-    client.close();
-});
+//     // perform actions on the collection object
+//     client.close();
+// });
+async function run() {
+    try {
+        await client.connect();
+        const inventoryItems = client.db('electronics-inventory-project-11').collection('items');
 
+        // To get / read all documents from database
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = inventoryItems.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+    }
+    finally {
+        // No code right now. Can be written if needed
+    }
+};
+run().catch(console.dir);
 
 
 // Root API : (getting / reading data form root)
