e4c6ebe93d5dcd4748ebec09cf322a376e9c5d46
shawon-MG <work.shawomg@gmail.com>
2022-05-08 22:39:47 +0600

    update cors

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 // middlewares:
-app.use(cors());
 app.use(express.json());
+// app.use(cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8chh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
